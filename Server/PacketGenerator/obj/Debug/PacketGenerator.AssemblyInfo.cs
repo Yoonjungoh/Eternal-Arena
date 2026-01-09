a721e6322ce2b9fb28e8e502623b49521f34f8b7
YoonJungOh <atcnature@gmail.com>
2026-01-10 02:01:53 +0900

    [feat] UI_Lobby의 RoomSubItem, UI_WaitingRoom UI 폴리싱

--- Server/PacketGenerator/obj/Debug/PacketGenerator.AssemblyInfo.cs
+++ Server/PacketGenerator/obj/Debug/PacketGenerator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PacketGenerator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd1b9a5b581368f5e85cdbc0ddbd38e6c654c4bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4983af3ffd4779f789e96f17e98b3f50fb4299ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("PacketGenerator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PacketGenerator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
